54eb06eb3ed2e69c60c03e228434f334308f44af
Kar Chun <karchuntan.1999@gmail.com>
2025-09-01 12:20:07 +0800

    test

---
+++ src/app/_meta.ts
@@ -0,0 +1,19 @@
+import type { MetaRecord } from 'nextra'
+
+/**
+ * type MetaRecordValue =
+ *  | TitleSchema
+ *  | PageItemSchema
+ *  | SeparatorSchema
+ *  | MenuSchema
+ *
+ * type MetaRecord = Record<string, MetaRecordValue>
+ **/
+const meta: MetaRecord = {
+  index: {
+    title: 'Home',
+    type: 'page'
+  }
+}
+
+export default meta
