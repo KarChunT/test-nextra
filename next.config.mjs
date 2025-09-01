ae79d21cb4d5a8a32657b9b5b6945d3e6ca7c0c4
Kar Chun <karchuntan.1999@gmail.com>
2025-09-01 11:51:53 +0800

    test

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from 'nextra'
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+})
+
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
+})
